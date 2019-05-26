3cfa89f3bc6a5848ac0d7ee5cf17611295475571
tammanache-nikhil <nikhil.tammanache33@gmail.com>
2019-05-27 00:31:26 +0530

    Initial project setup

---
+++ NikLog/NikLog/NikLog.h
@@ -0,0 +1,19 @@
+//
+//  NikLog.h
+//  NikLog
+//
+//  Created by Tammanache, Nikhil (US - Bengaluru) on 27/05/19.
+//  Copyright © 2019 Tammanache, Nikhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NikLog.
+FOUNDATION_EXPORT double NikLogVersionNumber;
+
+//! Project version string for NikLog.
+FOUNDATION_EXPORT const unsigned char NikLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NikLog/PublicHeader.h>
+
+
